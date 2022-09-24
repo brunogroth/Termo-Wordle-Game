f3b65fc06bf9cd9ec6a13769fe32dc0daa565808
Bruno Groth <brunom.groth@gmail.com>
2022-09-24 17:46:10 -0300

    Iniciado RecyclerView, corrigido string tentativa

--- app/src/main/java/com/example/termo_wordle/MainActivity.java
+++ app/src/main/java/com/example/termo_wordle/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.termo_wordle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -30,17 +31,23 @@ public class MainActivity extends AppCompatActivity {
         botaoEnviar = findViewById(R.id.enviar);
         tentativa = findViewById(R.id.tentativa);
 
+        //INTERFACE RECYCLER VIEW -
+        //Layout
+        // RecyclerView.LayoutManager = new GridLayoutManager(getApplicationContext());
+        // recyclerViewPalavras.setLayoutManager(GridLayoutManager);
+
         //Listener do botão Enviar
         botaoEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Aqui ocorrerá a validação da palavra enviada
 
+                String tentativaAtual = tentativa.toString();
                 //Gera uma palavra
                 String palavraSorteada = gerarPalavras();
                 
                 //valida a tentativa
-                int retorno = validarTentativa(tentativa, palavraSorteada);
+                boolean resultado = validarTentativa(tentativaAtual, palavraSorteada);
             }
         });
     }
@@ -54,17 +61,12 @@ public class MainActivity extends AppCompatActivity {
 
         return palavraSorteada;
     }
-    public int[] validarTentativa(String tentativa, String palavraSorteada){
-        int posicaoAcertos[] = new int[5];
 
-        //
-        //fazer um FOR que acessa cada letra da tentativa e verifica se é igual a alguma letra da palavra sorteada.
-        //a lógica precisa validar duas coisas: se a letra 1 da tentativa é igual a letra 1 da palavraSorteada e dar um retorno, e precisa validar se a letra 1 da tentativa é igual a qualquer letra da palavraSorteada e dar outro retorno. faça isso usando um for dentro do outro, o primeiro acessando for cada letra da palavra e o outro verificando as letras sao iguais.
-        //pensei em retornar um array de valores 0 1 e 2 onde 0 = nao achou a letra, 1 = letra certa e 2 = letra encontrada na palavra, mas precisa pensar melhor nisso
-
+    public boolean validarTentativa(String tentativa, String palavraSorteada){
+        boolean resultado = false;
         if(tentativa == palavraSorteada){
             //fim de jogo
         }
-        return posicaoAcertos;
+        return resultado;
     }
 }
