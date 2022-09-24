d0cbb2f2f2916ee8f5c41004100fa0f515e22a59
Bruno Groth <brunom.groth@gmail.com>
2022-09-24 14:45:09 -0300

    Declaração e instanciamento de objetos RecycleView, Botão e Input Text

--- app/src/main/java/com/example/termo_wordle/MainActivity.java
+++ app/src/main/java/com/example/termo_wordle/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.termo_wordle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaração
+    private RecyclerView recyclerViewPalavras;
+    private Button botaoEnviar;
+    private TextInputEditText tentativa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Instâncias
+        recyclerViewPalavras = findViewById(R.id.recyclerView);
+        botaoEnviar = findViewById(R.id.enviar);
+        tentativa = findViewById(R.id.tentativa);
+
     }
 }
